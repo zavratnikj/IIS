143931090323c0821023f91853c07014b4645097
zavratnikj <jure.zavratnik@student.um.si>
2023-03-09 17:04:06 +0100

    scheduled git Actions test

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -15,18 +15,18 @@ if response.status_code == 200:
 else:
     print(f"Error: {response.status_code}")
 
-API_key = "facbbd327383c3c420b0a461707dc933"
-history_data = "https://api.openweathermap.org/data/2.5/history/city?lat=46.55&lon=15.65&type=hour&start=",first_date_unix,"&end=",last_date_unix,"&appid=",API_key
-
-response = requests.get(history_data)
-
-if response.status_code == 200:
-    data = json.loads(response.text)
-    pprint.pprint(data)
-    with open('../../data/raw/weather/neobdelani_podatki.json', 'w') as f:
-        json.dump(data, f)
-else:
-    print(f"Error: {response.status_code}")
+# API_key = "facbbd327383c3c420b0a461707dc933"
+# history_data = "https://api.openweathermap.org/data/2.5/history/city?lat=46.55&lon=15.65&type=hour&start=",first_date_unix,"&end=",last_date_unix,"&appid=",API_key
+#
+# response = requests.get(history_data)
+#
+# if response.status_code == 200:
+#     data = json.loads(response.text)
+#     pprint.pprint(data)
+#     with open('../../data/raw/weather/neobdelani_podatki.json', 'w') as f:
+#         json.dump(data, f)
+# else:
+#     print(f"Error: {response.status_code}")
 
 
 # df.to_csv('../../data/processed/air/obdelani_podatki.csv')
