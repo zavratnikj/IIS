5907b284e2153c5193da43810a57a103e30bf669
zavratnikj <jure.zavratnik@student.um.si>
2023-03-09 23:16:53 +0100

    Popravljen test_api file

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -9,9 +9,13 @@ def client():
 
 def test_predict(client):
     data = {
-        "no2": 47,
-        "pm2.5": 15,
+        "no2": 43,
+        "pm2.5": 17,
         "benzen": 3,
+        "temp": 2,
+        "pressure": 987,
+        "humidity": 82,
+        "wind_speed": 2.76,
         "year": 2023,
         "hour": 19,
         "day": 15,
